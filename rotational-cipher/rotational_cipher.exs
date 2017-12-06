661e63285d3f70a0eaf9b23c0c5c9df7066fb35f
Gabe Hargrave <ghargrave@workwithopal.com>
2017-12-06 09:09:29 -0800

    Rotational Cipher complete

--- rotational-cipher/rotational_cipher.exs
+++ rotational-cipher/rotational_cipher.exs
@@ -1,4 +1,6 @@
 defmodule RotationalCipher do
+  @alpha "abcdefghijklmnopqrstuvwxyz"
+
   @doc """
   Given a plaintext and amount to shift by, return a rotated string.
 
@@ -7,7 +9,48 @@ defmodule RotationalCipher do
   "Nggnpx ng qnja"
   """
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
+  def rotate(text, 0) do
+    text
+  end
+
+  @spec rotate(text :: String.t(), shift :: integer) :: String.t()
+  def rotate(text, shift) when rem(shift, 26) == 0 do
+    text
+  end
+
+  @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
+    alpha_clean = String.split(@alpha, "", trim: true)
+    alpha_encoded = build_encoded(alpha_clean, rem(shift, 26))
+
+    text
+    |> String.split("", trim: true)
+    |> Enum.map(&(sub_character(alpha_clean, alpha_encoded, &1)))
+    |> Enum.join
+  end
+
+  defp sub_character(alpha_clean, alpha_encoded, char) do
+    unless String.match?(char, ~r{[A-z]}) do
+      char
+    else
+      # find index of original char,
+      # replace it with same index from 'alpha_encoded'
+      new_char = Enum.at(
+        alpha_encoded, 
+        Enum.find_index(alpha_clean, &(&1 == String.downcase(char)))
+      )
+
+      # convert back to upcase if original was upcase
+      case String.match?(char, ~r{[A-Z]}) do
+        true -> String.upcase(new_char)
+        false -> new_char
+      end
+    end
+  end
+
+  defp build_encoded(alpha_clean, shift) do
+    Enum.concat(
+      Enum.slice(alpha_clean, shift..26),
+      Enum.slice(alpha_clean, 0..(shift - 1)))
   end
 end
-
